66bfdac75093e7392ed9174763c89f523a6db23c
Boris Spektor <boris@soom.la>
2016-01-13 10:38:41 +0200

    Prepared release of v2.0.6

--- src/com/soomla/store/billing/amazon/AmazonIabService.java
+++ src/com/soomla/store/billing/amazon/AmazonIabService.java
@@ -37,7 +37,7 @@ import java.util.Map;
  */
 public class AmazonIabService implements IIabService {
 
-    public static final String VERSION = "2.0.5";
+    public static final String VERSION = "2.0.6";
 
     /**
      * see parent
